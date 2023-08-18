62aad71242c3c7bfe4d9b90397dec81b7e67400f
Sayan Karmakar <ironheartz367@gmail.com>
2023-08-19 00:20:34 +0530

    initial commit

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def get_investments_plot_by_price(data):
     qts = [qt['quantity'] for qt in data]
 
     fig1 = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
-    fig1.update_layout(plot_bgcolor='rgba(0,0,0,0)', title="<b>Investments per Company by Weightage:</b>")
+    fig1.update_layout(plot_bgcolor='rgba(0,0,0,0)', title="<b>Investments per Company by Price Weightage:</b>")
 
 
     df1 = pd.DataFrame(list(zip(labels, values)), columns=['Companies -->', 'Amounts -->'])
@@ -142,6 +142,54 @@ def get_investments_plot_by_price(data):
     st.markdown(f'Total Amount Invested: {sum(values):.2f} /-')
 
 
+def get_wannabe_investments_plot_by_price(data):
+    labels = [name['company_name'] for name in data]
+    values = [price['last_price']*(10 - price['quantity']) for price in data]
+    qts = [10 - qt['quantity'] for qt in data]
+
+    fig1 = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
+    fig1.update_layout(plot_bgcolor='rgba(0,0,0,0)', title="<b>Investments per Company by Price Weightage Required:</b>")
+
+
+    df1 = pd.DataFrame(list(zip(labels, values)), columns=['Companies -->', 'Amounts -->'])
+    df1.sort_values(by=['Amounts -->'], inplace=True)
+    fig2 = px.bar(
+        df1,
+        x = 'Amounts -->',
+        y = 'Companies -->',
+        orientation="h",
+        title="<b>Investments per Company by Amount Required:</b>",
+    )
+    fig2.update_layout(
+        plot_bgcolor="rgba(0,0,0,0)",
+        xaxis=(dict(showgrid=True))
+    )
+
+    df2 = pd.DataFrame(list(zip(labels, qts)), columns=['Companies -->', 'Quantity -->'])
+    df2.sort_values(by=['Quantity -->'], inplace=True)
+    fig3 = px.bar(
+        df2,
+        x = 'Quantity -->',
+        y = 'Companies -->',
+        orientation="h",
+        title="<b>Shares per Company by Quantity Required:</b>",
+    )
+    fig3.update_layout(
+        plot_bgcolor="rgba(0,0,0,0)",
+        xaxis=(dict(showgrid=True))
+    )
+
+    st.subheader('Distribution by Amount Required')
+    l, r = st.columns(2)
+    with l:
+        st.plotly_chart(fig1, use_container_width=True)
+    with r:
+        st.plotly_chart(fig3, use_container_width=True)
+
+    st.plotly_chart(fig2, use_container_width=True)
+    st.markdown(f'Total Amount Required: {sum(values):.2f} /-')
+
+
 
 response = st.experimental_get_query_params()
 if 'code' in response:
@@ -149,11 +197,15 @@ if 'code' in response:
     login(response['code'][0])
     st.success('Login Successfull!')
 
-    holdings = st.button('Show Holdings')
-    if holdings:
-        data = get_holdings()
-        st.write(data)
+    data = get_holdings()
+    st.write(data)
+
+    with st.expander('Show Holdings'):
         get_investments_plot_by_price(data['data'])
+        st.markdown('##')
+    
+    with st.expander('Show Goals'):
+        get_wannabe_investments_plot_by_price(data['data'])
         st.markdown('##')
 
 else:
