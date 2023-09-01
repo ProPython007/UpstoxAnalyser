e45a10daf53d616dda2a09b781d4a8c664437b7c
Sayan Karmakar <ironheartz367@gmail.com>
2023-09-01 15:50:29 +0530

    initial commit

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def setup_config(code):
 
 
 def connect(conf):
-    redirect_url = urllib.parse.quote(conf['rurl'], safe='')
+    # redirect_url = urllib.parse.quote(conf['rurl'], safe='')
     uri = f"https://api-v2.upstox.com/login/authorization/dialog?response_type=code&client_id={conf['apiKey']}&redirect_uri={redirect_url}"
 
     st.markdown(f'[Authorize with Upstox]({uri})')
